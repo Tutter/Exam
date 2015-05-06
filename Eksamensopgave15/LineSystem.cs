66cbca3fef16f96d3dd12ce385afa92968c7588c
Tutter <tuttanium@gmail.com>
2015-05-06 18:11:49 +0200

    Added LineSystem.cs

---
+++ Eksamensopgave15/LineSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensopgave15
+{
+    class LineSystem
+    {
+        public void BuyProduct()
+        {
+
+        }
+
+        public void AddCreditsToAccount()
+        {
+
+        }
+
+        public void ExecuteTransaction()
+        {
+
+        }
+
+        public void GetProduct()
+        {
+
+        }
+
+        public void GetUser()
+        {
+
+        }
+
+        public void GetTransactionList()
+        {
+
+        }
+
+        public void GetActiveProducts()
+        {
+
+        }
+    }
+}
